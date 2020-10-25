836f74a81a69ffe09f03e5fab06510ef9a8714d9
CastleJaeMin <ehdrud1332@gmail.com>
2020-10-25 20:29:58 +0900

    cacheFonts, Images

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text} from 'react-native';
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo';
+import {Asset} from 'expo-asset';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const cacheImages = images =>
+    images.map(image => {
+        if(typeof image === 'string') {
+            return Image.prefetch(image);
+        } else {
+            return Asset.fromModule(image).downloadAsync();
+        }
+    });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const cacheFonts = fonts =>
+    fonts.map(font => [Font.loadAsync(font), Font.loadAsync(font)]);
+
+const App = () => {
+
+    const [isReady, setIsReady] = useState(false)
+
+    const loadAssets = async() => {
+        const images = cacheImages([
+            "https://images.unsplash.com/photo-1571847140471-1d7766e825ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=673&q=80",
+            require("./assets/splash.png")
+        ]);
+        console.log(images);
+        const fonts = cacheFonts([Ionicons.font]);
+        return Promise.all([...images, ...fonts]);
+    }
+    const onFinish = () => setIsReady(true);
+
+    return isReady ? (
+        <View>
+          <Text>
+            asdfasdf
+          </Text>
+        </View>
+    ) : (
+        <AppLoading
+            startAsync={loadAssets}
+            onFinish={onFinish}
+            onError={console.log}
+        />
+    )
+};
+
+export default App;
